7a3f0dac2903050d9c66db29949860a0e5157436
hp <jatingulati1264@gmail.com>
2026-01-11 16:28:59 +0530

    fixed issues and added bootstrap fix

--- app.py
+++ app.py
@@ -155,11 +155,11 @@ def logout():
         redirect('/users/login')
     return redirect('/users/login')
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     # This block creates the empty todo.db and tables if they don't exist
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=5000)
 
 
