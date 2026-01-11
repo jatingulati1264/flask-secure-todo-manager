946a118bb79d5ee7a8d62c3b0c9ed4c3e1747702
hp <jatingulati1264@gmail.com>
2026-01-11 15:44:59 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
 
 # To create this database just use below commands in python interpreter.
